324baca6e6a9e61833768069fdf7b4e9de788d16
Viraj Dhumal SPIT <mevirajdhumal@gmail.com>
2023-02-01 10:56:22 +0530

    Added styles

--- public/js/client.js
+++ public/js/client.js
@@ -11,14 +11,26 @@ const append = (message, position) => {
     messageContainer.append(messageElement);
 }
 
-const handleUserJoined = (username) => {
+const handleUserJoined = (message) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerHTML = message;
+    messageElement.classList.add('centerUserConnection');
+    messageElement.style.color='black';
+    messageElement.style.backgroundColor='rgb(55, 255, 55)';
+    messageContainer.append(messageElement);
+}
 
+const handleUserLeave= (message) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerHTML = message;
+    messageElement.classList.add('centerUserConnection');
+    messageContainer.append(messageElement);
 }
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     const message = messageInput.value;
-    append(`You: ${message}`, 'right');
+    append(`<strong>You</strong>: ${message}`, 'right');
     socket.emit('send', message);
     messageInput.value = "";
 });
@@ -26,14 +38,14 @@ form.addEventListener('submit', (e)=>{
 const username = prompt("Enter your name to join");
 socket.emit('new-user-joined', username);
 
-socket.on('user-joined', username=> {
-    append(`<strong>${username}</strong> joined the chat`, 'right');
+socket.on('user-joined', message=> {
+    handleUserJoined(`<strong>${message}</strong> joined the chat!`, 1);
 });
 
 socket.on('receive', data => {
-    append(`${data.username}: ${data.message}`, 'left');
+    append(`<strong>${data.username}</strong>: ${data.message}`, 'left');
 });
 
 socket.on('leave', data => {
-    append(`${data.username} has left the chat!`, 'right');
+    handleUserLeave(`${data.username} has left the chat!`);
 });
